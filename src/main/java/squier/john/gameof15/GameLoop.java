dd7db72299e6836c3bc89f886067d053e208a1bf
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-22 16:31:50 -0500

    initial commit

---
+++ src/main/java/squier/john/gameof15/GameLoop.java
@@ -0,0 +1,8 @@
+package squier.john.gameof15;
+
+/**
+ * Created by johnsquier on 1/22/17.
+ */
+public class GameLoop {
+
+}
---
+++ src/test/java/squier/john/gameof15/GameLoopTest.java
@@ -0,0 +1,7 @@
+package squier.john.gameof15;
+
+/**
+ * Created by johnsquier on 1/22/17.
+ */
+public class GameLoopTest {
+}
