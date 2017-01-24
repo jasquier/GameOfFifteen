f232eca343531e02d27bcf749abd1b8aaff94710
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-23 19:21:06 -0500

    worked out game loop comments

--- src/main/java/squier/john/gameof15/GameLoop.java
+++ src/main/java/squier/john/gameof15/GameLoop.java
@@ -5,4 +5,19 @@ package squier.john.gameof15;
  */
 public class GameLoop {
 
+    // get game board size
+    // check if it's in range
+
+    // intro screen
+
+    // initialize the board
+
+    // game loop
+        // clear screen
+        // draw board in current state
+        // check for win and break
+        // prompt for move
+        // if ( !move (inputTile) ) { /* runs if a move is not possible */ }
+            // move updates board state
+
 }
--- src/test/java/squier/john/gameof15/GameLoopTest.java
+++ src/test/java/squier/john/gameof15/GameLoopTest.java
@@ -4,4 +4,5 @@ package squier.john.gameof15;
  * Created by johnsquier on 1/22/17.
  */
 public class GameLoopTest {
+
 }
