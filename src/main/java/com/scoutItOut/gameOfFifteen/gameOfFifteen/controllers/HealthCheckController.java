10c6e56d1b29458d5601c55c8d9a2919876cdf87
jasquier <jasquier@gmail.com>
2017-07-29 22:48:38 -0400

    added some tests to the chaged HealthCheckController

--- src/main/java/com/scoutItOut/gameOfFifteen/gameOfFifteen/controllers/HealthCheckController.java
+++ src/main/java/com/scoutItOut/gameOfFifteen/gameOfFifteen/controllers/HealthCheckController.java
@@ -6,8 +6,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HealthCheckController {
 
+    private static final String template = "Hello, %s!";
+
     @RequestMapping(value = "/healthCheck", method = RequestMethod.GET)
-    public String getHealthCheck() {
-        return "I'm alive!";
+    public HealthResponse getHealthCheck(@RequestParam(value = "content", defaultValue = "World") String content) {
+        return new HealthResponse(String.format(template, content));
+    }
+
+    class HealthResponse {
+        private String content;
+
+        HealthResponse(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
     }
 }
---
+++ src/test/java/com/scoutItOut/gameOfFifteen/gameOfFifteen/controllers/HealthCheckControllerTest.java
@@ -0,0 +1,40 @@
+package com.scoutItOut.gameOfFifteen.gameOfFifteen.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void noParamHealthCheckShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/healthCheck"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void paramHealthCheckShouldReturnTailoredMessage() throws Exception {
+        this.mockMvc.perform(get("/healthCheck")
+                .param("contebt", "Unit Testing"))
+                .andDo(print()).
+                andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Unit Testing!"));
+    }
+}
