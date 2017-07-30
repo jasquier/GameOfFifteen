926e81d6e2309cb09b8412757e961f54bfaf1354
jasquier <jasquier@gmail.com>
2017-07-29 21:02:03 -0400

    Initial Spring Initializer project added.

---
+++ src/main/java/com/scoutItOut/gameOfFifteen/gameOfFifteen/GameOfFifteenApplication.java
@@ -0,0 +1,12 @@
+package com.scoutItOut.gameOfFifteen.gameOfFifteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfFifteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfFifteenApplication.class, args);
+	}
+}
